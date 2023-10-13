2a13bf4dd4d3985ac3767abe6c3d0641e29fc1b6
alessandro baiocco <baioccoalessandro97@gmail.com>
2023-10-13 10:10:43 +0200

    creati classi con costruttori set and get

---
+++ src/main/java/catalog/Material.java
@@ -0,0 +1,42 @@
+package catalog;
+
+public abstract class Material {
+
+    private int ISBN;
+    private String titolo;
+    private int annoDiPubblicazione;
+    private int numeroPagine;
+
+
+    public int getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(int ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public int getAnnoDiPubblicazione() {
+        return annoDiPubblicazione;
+    }
+
+    public void setAnnoDiPubblicazione(int annoDiPubblicazione) {
+        this.annoDiPubblicazione = annoDiPubblicazione;
+    }
+
+    public int getNumeroPagine() {
+        return numeroPagine;
+    }
+
+    public void setNumeroPagine(int numeroPagine) {
+        this.numeroPagine = numeroPagine;
+    }
+}
